679e9db518e2cc3c5709efd7f5e1fec5b8a927c8
John Yi <john.yi1411@gmail.com>
2019-11-17 23:51:29 -0800

    Init

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# Architecture
+Create flow diagram for a submitted command
+Create data structure for commands
+Create schema
+Create front end
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <p>cd ..</p>
+    </div>
+  );
+}
+
+export default App;
